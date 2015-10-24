7855b72cbce6f80ebd645721180128daafe20b36
Gilberto Trindade <g.trindade@gmail.com>
2015-10-24 20:24:22 -0300

    add optk e public como excluded subdomain

--- config/initializers/apartment/subdomain_exclusions.rb
+++ config/initializers/apartment/subdomain_exclusions.rb
@@ -1,3 +1,3 @@
 # config/initializers/apartment/subdomain_exclusions.rb
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'www.optk']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'www.optk' 'optk' 'public']
 #Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
