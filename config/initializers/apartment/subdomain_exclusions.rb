5c9165ab88ac700a203c7aeac35d14ab56da7684
Gilberto Trindade <g.trindade@gmail.com>
2015-10-24 20:56:56 -0300

    firstSubdomain

--- config/initializers/apartment/subdomain_exclusions.rb
+++ config/initializers/apartment/subdomain_exclusions.rb
@@ -1,3 +1,5 @@
 # config/initializers/apartment/subdomain_exclusions.rb
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'www.optk' 'optk' 'public']
+#Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'www.optk' 'optk' 'public']
+require 'apartment/elevators/first_subdomain'
+Apartment::Elevators::FirstSubdomain.excluded_subdomains = ['staging', 'www']
 #Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
