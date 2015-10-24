cb52e40cbe174e6a2a71c6aca8452da5bea8997f
Gilberto Trindade <g.trindade@gmail.com>
2015-10-24 19:16:12 -0300

    codigo anterior... so foi deixado o apartment/subdomain_exclusions.rb

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -2,8 +2,4 @@ require 'apartment/elevators/subdomain'
 
 Apartment.configure do |config|
 	config.excluded_models = ['Account']
-
-	Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
-
-	Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
 end
