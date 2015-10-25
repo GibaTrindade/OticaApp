cc8692d9b73001efec7602b945b61b695f57caea
Gilberto Trindade <g.trindade@gmail.com>
2015-10-24 21:06:17 -0300

    Subdomain

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 
 module OticaApp
   class Application < Rails::Application
-    #config.middleware.use 'Apartment::Elevators::Subdomain'
-    config.middleware.use 'Apartment::Elevators::FirstSubdomain'
+    config.middleware.use 'Apartment::Elevators::Subdomain'
+    #config.middleware.use 'Apartment::Elevators::FirstSubdomain'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/apartment/subdomain_exclusions.rb
+++ config/initializers/apartment/subdomain_exclusions.rb
@@ -1,5 +1,6 @@
 # config/initializers/apartment/subdomain_exclusions.rb
-#Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'www.optk' 'optk' 'public']
-require 'apartment/elevators/first_subdomain'
-Apartment::Elevators::FirstSubdomain.excluded_subdomains = ['staging', 'www']
+require 'apartment/elevators/subdomain'
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'staging' 'optk' 'public']
+#require 'apartment/elevators/first_subdomain'
+#Apartment::Elevators::FirstSubdomain.excluded_subdomains = ['staging', 'www']
 #Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
