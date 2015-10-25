9b237e7a7354084549683a343e7aeab8a347e86c
Gilberto Trindade <g.trindade@gmail.com>
2015-10-24 21:17:04 -0300

    comentado subdomain em aplication.rb

--- config/initializers/apartment/subdomain_exclusions.rb
+++ config/initializers/apartment/subdomain_exclusions.rb
@@ -1,6 +1,6 @@
 # config/initializers/apartment/subdomain_exclusions.rb
 require 'apartment/elevators/subdomain'
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'staging' 'optk' 'public']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www' 'staging' 'public']
 #require 'apartment/elevators/first_subdomain'
 #Apartment::Elevators::FirstSubdomain.excluded_subdomains = ['staging', 'www']
 #Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
